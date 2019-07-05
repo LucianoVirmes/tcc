163d735643d548de4470c8b50e0b1f630ae37c09
LucianoVirmes <luciano.virmes18@gmail.com>
2019-07-05 00:32:46 -0300

    adicionado bootstrap ao projeto e removido alguns pacotes desnecessarios

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  
+    <form method="post">
+        <label>Nome de usuário</label>
+        <input type="text" name="usuario.nome"/>
+        <button type="submit">Enviar</button>
+    </form>
 </template>
 
 <style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,9 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './registerServiceWorker'
+import Vue from 'vue';
+import App from './App.vue';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import router from './router';
+import './registerServiceWorker';
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
