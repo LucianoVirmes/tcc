4196d02a8b9d7c7b867fd4e8c1112a88f18e73a2
LucianoVirmes <luciano.virmes18@gmail.com>
2019-07-07 19:21:10 -0300

    adicionado sequelize para conexão com servidor do postgres

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
     const app = require('./src/config/express');
-
+    const db =  require('./src/config/dataBase');
     const rotas = require('./src/app/rotas/rota1');
     //define rotas
     rotas(app);
+    db
+    .authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+        })
+        .catch(err => {
+            console.error('Unable to connect to the database:', err);
+        });
+        
     app.listen(process.env.PORT || 3000);
---
+++ server/src/config/dataBase.js
@@ -0,0 +1,5 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize
+('postgres://gvxzsdrj:icq4cTPB5ZSVOW_YZoqXa86LY_9BQbvI@motty.db.elephantsql.com:5432/gvxzsdrj');
+
+  module.exports = sequelize;
