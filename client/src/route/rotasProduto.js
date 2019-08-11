b6b57dc69800e26316b00693967cc0bacf2a5238
LucianoVirmes <luciano.virmes18@gmail.com>
2019-08-10 22:54:52 -0300

    adicionada meta as rotas de produtos

--- client/src/route/rotasProduto.js
+++ client/src/route/rotasProduto.js
@@ -2,9 +2,10 @@ import Cadastro from '../views/produto/Cadastro.vue';
 import Lista from '../views/produto/Lista.vue';
 import Produto from '../views/produto/Index.vue';
 
+const meta = { desc: "Produtos" }
 const lista = { path: '/produto/lista', titulo: 'Lista', component: Lista, meta };
 const cadastro = { path: '/produto/cadastro', titulo: 'Cadastro', component: Cadastro, meta }
-const meta = { desc: "Produtos" }
+
 export const rotasProduto =
 {
     path: '/produto/lista', component: Produto, titulo: 'Produtos', meta, children: [lista, cadastro]
